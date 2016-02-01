68c399540217940ee96fe68780134164cb018d44
boris_chantel <ivalicle@gmail.com>
2016-02-01 00:13:53 +0000

    First commit

---
+++ myscripts.js
@@ -0,0 +1,19 @@
+/*$(document).ready (function() {
+    
+var r = ["You have to learn the rules of the game. And then you have to play better than anyone else"];
+var r2 = ["Look deep into nature, and then you will understand everything better.Albert Einstein"]
+var r3 = ["Try not to become a man of success, but rather try to become a man of value."]
+*/
+//$("#whatever").text(r2);
+
+
+
+    
+    
+//}); //end ready
+var r = ["You have to learn the rules of the game. And then you have to play better than anyone else", "Look deep into nature, and then you will understand everything better.Albert Einstein", "Try not to become a man of success, but rather try to become a man of value."];
+
+
+
+document.getElementById('whatever').innerHTML = r[0];
+
