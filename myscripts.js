cec5afe6b2096c44baa87a7e5da125aaa4d53da0
boris_chantel <ivalicle@gmail.com>
2016-02-02 03:42:46 +0000

    Generator works now

--- myscripts.js
+++ myscripts.js
@@ -1,19 +1,50 @@
-/*$(document).ready (function() {
+$(document).ready (function() {
+    var r = ["You have to learn the rules of the game. And then you have to play better than anyone else", "Look deep into nature, and then you will understand everything better.Albert Einstein", "Try not to become a man of success, but rather try to become a man of value."];
+    var temp = -1;
     
+   
+        $("button").on('click', function() {
+            
+            var x = Math.floor(Math.random() * 3);
+                
+                while (x === temp) {
+                        x = Math.floor(Math.random() * 3);
+                    }
+                        
+                $("#quote").text(r[x]);
+                temp = x;
+         
+        });
+    
+    
+}); //end ready
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 var r = ["You have to learn the rules of the game. And then you have to play better than anyone else"];
 var r2 = ["Look deep into nature, and then you will understand everything better.Albert Einstein"]
 var r3 = ["Try not to become a man of success, but rather try to become a man of value."]
 */
+
+//document.getElementById('quote').innerHTML = r[0];
+
+
 //$("#whatever").text(r2);
-
-
-
-    
-    
-//}); //end ready
-var r = ["You have to learn the rules of the game. And then you have to play better than anyone else", "Look deep into nature, and then you will understand everything better.Albert Einstein", "Try not to become a man of success, but rather try to become a man of value."];
-
-
-
-document.getElementById('whatever').innerHTML = r[0];
-
